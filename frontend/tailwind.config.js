f245693d58f16961d124873a61b46c0232208ba0
vasu kamani <vasukamani.ce@gmail.com>
2025-01-07 00:09:48 +0530

    require

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
     darkMode: ["class"],
     content: [
@@ -59,6 +60,6 @@ export default {
     		}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
   }
   
