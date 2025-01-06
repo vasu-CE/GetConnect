ea9abb7cdbd8ae5ec099277e09acf871d9e662d6
vasu kamani <vasukamani.ce@gmail.com>
2025-01-07 00:16:05 +0530

    userAuth.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,8 @@ import ProfilePage from './components/ProfilePage';
 import Quiz from './components/Quiz';
 import Projects from './components/Projects';
 import ProjectPage from './components/ProjectPage';
-import UserAuth from './auth/userAuth';
+// import UserAuth from './auth/userAuth';
+import UserAuth from './auth/UserAuth.jsx';
 
 // Create a wrapper component to conditionally render Navbar
 const NavbarWrapper = () => {
