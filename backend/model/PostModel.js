f7f941668a5db71657b79082350f1195f5e03b91
vasu kamani <vasukamani.ce@gmail.com>
2025-02-08 10:42:01 +0530

    model update for comment

--- backend/model/PostModel.js
+++ backend/model/PostModel.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
 const postSchema = mongoose.Schema({
     caption : {
         type : String,
@@ -13,6 +20,7 @@ const postSchema = mongoose.Schema({
         ref: 'User',
         required: true
     },
+    comments: [commentSchema], 
     likes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
